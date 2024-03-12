b014e1708fd7b135aa3c9fb13ce4f1c3973b5584
IsItIzzet <ohlhjames@gmail.com>
2024-03-12 17:54:56 -0500

    added starter GET code to notes.js also added utilFS to helpers so I can Read and write to the database

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,30 @@
 const notes = require('express').Router();
+const { readFromFile} = require('../helpers/fsUtils');
+
 notes.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+notes.post('/', (req, res) =>{
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = notes
