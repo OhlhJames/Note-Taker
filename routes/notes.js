435faf8a1a119788c28277ff99a2ad05a99648e5
IsItIzzet <ohlhjames@gmail.com>
2024-03-13 16:25:31 -0500

    added comments to code sections

--- routes/notes.js
+++ routes/notes.js
@@ -2,11 +2,16 @@ const notes = require('express').Router();
 const {readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-notes.get('/', (req, res) =>
+// utilizes the get fetch in front end so that notes can be displayed from data base
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-);
+  
+});
 
+// utilizes the post fetch in front end so that users can post notes
 notes.post('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
   const { title, text } = req.body;
 
   if (title && text) {
@@ -25,7 +30,7 @@ notes.post('/', (req, res) => {
 
     res.json(response);
   } else {
-    res.json('Error in posting feedback');
+    res.json('Error in posting note');
   }
   
 });
--- server.js
+++ server.js
@@ -10,14 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', api)
 
+// serves home page when users request an empty navigation
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
+// serves notes page when users request a notes navigation
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html')));
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
+// listens for if the server is running and informs user of port it is running on as well as providing a link to users
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
